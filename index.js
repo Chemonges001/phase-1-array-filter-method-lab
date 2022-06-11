689be2fb514b34453f1e5528223100e224b862e2
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-11 20:16:59 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const { arrayContaining } = require("expect");
+
 // Code your solution here
+function findMatching (driver, name) {
+    return driver.filter(function(newDriver){
+        return newDriver.toLowerCase() === name.toLowerCase()
+    });
+}
+
+function fuzzyMatch (drivers, string) {
+    return drivers.filter(function(newDriver){
+        return newDriver.slice(0, 1) === string.slice(0,1)
+    })
+}
+function matchName( driver, name) {
+    return driver.filter(function(driver){
+        return driver.name === name
+    })
+}
